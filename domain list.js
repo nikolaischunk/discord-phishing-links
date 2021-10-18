38715ff4bf45e18a62510b75f8ea0e62f916b30f
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2021-10-18 16:07:27 +0200

    update

--- domain list.js
+++ domain list.js
@@ -2,10 +2,11 @@ const suspiciousDomains = [
   'https://disordapp.codes',
   'https://discord-gifte.com',
   'https://dlscorcl.gift',
-  "https://dlscord.net",
-  "https://dlscocrdapp.com",
-  "https://discordd.gift",
-  "https://discord-apps.fun",
-  "https://dlscorcl.pw",
-  "https://discoclapp.pw"
+  'https://dlscord.net',
+  'https://dlscocrdapp.com',
+  'https://discordd.gift',
+  'https://discord-apps.fun',
+  'https://dlscorcl.pw',
+  'https://discoclapp.pw',
+  'http://discordc.gift',
 ]
